2896954646b6ece89459164e89a630f56774732e
Tyler Walke <tyler.walke@gmail.com>
2018-11-30 08:23:15 -0700

    Projects
    In class projects

---
+++ if.js
@@ -0,0 +1,74 @@
+//Prelims
+if(5>3) {
+  console.log("is greater than");
+}
+
+if("cat" == 3) {
+  console.log("is the length");
+} else {
+  console.log("is not length");
+}
+
+if("cat" == "dog") {
+  console.log("same");
+} else {
+  console.log("not the same");
+}
+
+
+// Bronze
+var person = {
+  name: "Bobby",
+  age: 12
+}
+
+if(person.age >= 18) {
+  console.log(person.name + " is allowed to go to the movie");
+}
+
+if(person.name.startsWith("B")) {
+  console.log(person.name + " allowed in movie");
+} else {
+  console.log(person.name + " not allowed in movie");
+}
+
+if(person.age >= 18 && person.name.startsWith("B")) {
+  console.log(person.name + " allowed in movie");
+} else {
+  console.log(person.name + " not allowed in movie");
+}
+
+// Silver
+if(1 == "1") {
+  console.log("strict");
+} else if(1 == "1") {
+  console.log("abstract");
+} else {
+  console.log("not equal at all");
+}
+
+if(1 <= 2 && 2 == 4) {
+  console.log("yes");
+} else {
+  console.log("no");
+}
+
+// Gold
+if(typeof("dog") == "string") {
+  console.log("string");
+} else {
+  console.log("not a string");
+}
+
+if (typeof(true) == "boolean") {
+  console.log("boolean");
+} else {
+  console.log("failed");
+}
+
+var notDefined;
+if (notDefined == undefined) {
+  console.log("undefined");
+} else {
+  console.log("defined");
+}
