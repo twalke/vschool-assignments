d9103b5ff59ebbb49b23132f0554701aef530288
Tyler Walke <tyler.walke@gmail.com>
2018-12-05 17:32:01 -0700

    Social.js
    Social.js done, loops and arrays in progress

---
+++ loopsAndArrays.js
@@ -0,0 +1,18 @@
+var hightestNum = 11;
+function createEvenArray(highestNum) {
+  var evensOnlyArray = [];
+  for (var i = 0; i < highestNum; i++) {
+    if (i%2 == 0) {
+      evensOnlyArray += i;
+    }
+  }
+  return evensOnlyArray;
+  console.log(evensOnlyArray);
+}
+
+
+function addOdds(evensOnlyArray) {
+  for (var i = 0; i < array.length; i++) {
+    array[i]
+  }
+}
---
+++ social.js
@@ -0,0 +1,67 @@
+var theMountainFriends = {
+    name: "Denali",
+    age: 25,
+    favoriteClimbers: [],
+    mountainFriends: [
+        {
+            name: "Everest",
+            age: 30,
+            favoriteClimbers: [
+                {
+                    climbers: "Hillary"
+                }
+            ]
+        },
+        {
+          name: "Timpanogos",
+          age: 20,
+          favoriteClimbers: [
+            {
+              climbers: "John Ute"
+            }
+          ]
+        }
+    ],
+    name: "Everest",
+    age: 30,
+    favoriteClimbers: [],
+    mountainFriends: [
+      {
+        name: "Timpanogos",
+        age: 20,
+        favoriteClimbers: [
+          {
+            climbers: "John Ute"
+          }
+        ]
+      },
+      {
+        name: "Denali",
+        age: 25,
+        favoriteClimbers: []
+      }
+    ],
+    name: "Timpanogos",
+    age: 20,
+    favoriteClimbers: [
+      {
+        climbers: "John Ute"
+      }
+    ],
+    mountainFriends: [
+      {
+        name: "Denali",
+        age: 25,
+        favoriteClimbers: []
+      },
+      {
+          name: "Everest",
+          age: 30,
+          favoriteClimbers: [
+              {
+                  climbers: "Hillary"
+              }
+          ]
+      }
+    ]
+}
